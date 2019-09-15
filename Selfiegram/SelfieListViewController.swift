9b1fd5d698bab154447b3b126aa5f36cebd84ccf
Mike Liao <mike.hm.liao@gmail.com>
2019-09-15 23:34:45 +0800

    implemented adding a new selfie

--- Selfiegram/SelfieListViewController.swift
+++ Selfiegram/SelfieListViewController.swift
@@ -36,6 +36,10 @@ class SelfieListViewController: UITableViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        
+        let addSelfieButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(createNewSelfie))
+        
+        navigationItem.rightBarButtonItem = addSelfieButton
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -74,7 +78,42 @@ class SelfieListViewController: UITableViewController {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let selfieToRemove = selfies[indexPath.row]
+            
+            do {
+                try SelfieStore.shared.delete(selfie: selfieToRemove)
+                
+                selfies.remove(at: indexPath.row)
+                
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            } catch {
+                let title = selfieToRemove.title
+                showError(message: "Failed to delete \(title).")
+            }
+        }
+    }
+    
+    @objc func createNewSelfie() {
+        let imagePicker = UIImagePickerController()
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            
+            if UIImagePickerController.isCameraDeviceAvailable(.front) {
+                imagePicker.cameraDevice = .front
+            } else {
+                imagePicker.sourceType = .photoLibrary
+            }
+            
+        }
+        imagePicker.delegate = self
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
     func showError(message: String) {
         let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
         
@@ -86,3 +125,37 @@ class SelfieListViewController: UITableViewController {
     
 }
 
+extension SelfieListViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage ?? info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            let message = "Couldn't get a picture from the image picker!"
+            showError(message: message)
+            return
+        }
+        
+        self.newSelfieTaken(image: image)
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func newSelfieTaken(image: UIImage) {
+        let newSelfie = Selfie(title: "New Selfie")
+        
+        newSelfie.image = image
+        
+        do {
+            try SelfieStore.shared.save(selfie: newSelfie)
+        } catch let error {
+            showError(message: "Can't save photo: \(error)")
+            return
+        }
+        
+        selfies.insert(newSelfie, at: 0)
+        
+        tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+    }
+}
